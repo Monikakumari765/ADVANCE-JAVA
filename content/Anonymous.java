db54d9d7a1d43464b7f23d6e6c907832e06b948a
Monika kumari <monikakumari4687@gmail.com>
2026-07-23 13:42:39 +0530

    practice

---
+++ content/Anonymous.java
@@ -0,0 +1,22 @@
+interface Robot{
+    void work();
+}
+
+public class Anonymous{
+
+    public static void main(String[] args){
+
+        Robot r = new Robot(){
+
+            @Override
+            public void work(){
+                System.out.println("Robot is Dancing");
+            }
+
+        };
+
+        r.work();
+
+    }
+
+}
