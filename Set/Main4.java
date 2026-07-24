ef13d8166099abf3d841efa8a34c2379260ba495
Monika kumari <monikakumari4687@gmail.com>
2026-07-24 18:58:47 +0530

    set4

---
+++ Set/Main4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        System.out.println(set.size());
+    }
+}
