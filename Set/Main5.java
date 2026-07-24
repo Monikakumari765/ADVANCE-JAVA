e55d7c030bc085a48197b6be2e32c16135b96dca
Monika kumari <monikakumari4687@gmail.com>
2026-07-24 18:59:39 +0530

    set5

---
+++ Set/Main5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main5 {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        System.out.println(set.isEmpty());
+
+        set.add(10);
+
+        System.out.println(set.isEmpty());
+    }
+}
