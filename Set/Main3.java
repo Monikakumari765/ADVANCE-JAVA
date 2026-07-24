d152a677b8e31ae33527b169b6829acb2d6c17aa
Monika kumari <monikakumari4687@gmail.com>
2026-07-24 18:58:06 +0530

    set3

---
+++ Set/Main3.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(10);
+        set.add(20);
+
+        set.remove(10);
+
+        System.out.println(set);
+    }
+}
