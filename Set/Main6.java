69eaeae68423cc1275b381aba22d1e00371588c8
Monika kumari <monikakumari4687@gmail.com>
2026-07-25 17:41:17 +0530

    practice

---
+++ Set/Main6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(10);
+        set.add(20);
+
+        set.clear();
+
+        System.out.println(set);
+    }
+}
