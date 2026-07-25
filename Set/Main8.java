8480e808128b4a48032143ff80e5c40df0d572b1
Monika kumari <monikakumari4687@gmail.com>
2026-07-25 17:43:08 +0530

    set8

---
+++ Set/Main8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main8 {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+
+        set.add(10);
+        set.add(20);
+        set.add(30);
+
+        Iterator<Integer> it = set.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
