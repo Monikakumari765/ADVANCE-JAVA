22e54cc99729b182a327b60eb82ae917bfdd82ba
Monika kumari <monikakumari4687@gmail.com>
2026-07-30 19:36:55 +0530

    1add

---
+++ collectiopractice.java/AddExample.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class AddExample {
+    public static void main(String[] args) {
+
+        HashSet<String> set = new HashSet<>();
+
+        set.add("Apple");
+        set.add("Banana");
+        set.add("Orange");
+
+        System.out.println(set);
+    }
+}
