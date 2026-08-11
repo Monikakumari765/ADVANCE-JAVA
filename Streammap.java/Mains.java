a3e6f9386957e4b45d930014a547ef1263b30e94
Monika kumari <monikakumari4687@gmail.com>
2026-08-11 11:56:12 +0530

    practice

--- Streammap.java/Mains.java
+++ Streammap.java/Mains.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Main11 {
+public class Mains {
     public static void main(String[] args) {
 
         List<Integer> nums = Arrays.asList(10, 20, 30, 40, 50, 60);
