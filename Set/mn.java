93ae84001ad4be0c2a009c04d8caf370e2aed189
Monika kumari <monikakumari4687@gmail.com>
2026-08-11 11:40:01 +0530

    practice

---
+++ Set/mn.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class mn {
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(10, 20, 30, 40, 50, 60);
+
+        Stream<Integer> evenNums = nums.stream().filter(n -> n > 25);
+
+        evenNums.forEach(System.out::println);
+    }
+}
