09d58559a9ce0e2d4c7ecd8544102216bf29fdfc
Monika kumari <monikakumari4687@gmail.com>
2026-08-24 11:49:00 +0530

    practice

---
+++ Terminals_operations.java/thread.java
@@ -0,0 +1,26 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
+
+public class thread {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+    
+
+        t1.start();
+        
+    }
+}
+// Thread
+//   ↓
+// Create Thread
+//   ↓
+// Override run()
+//   ↓
+// Create object
+//   ↓
+// Call start()
+//   ↓
+// Thread starts execution
