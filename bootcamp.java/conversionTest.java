a952337f5aeb8001d8bea3a4b0280f9015ff5d13
Monika kumari <monikakumari4687@gmail.com>
2026-09-03 09:10:49 +0530

    practice

--- bootcamp.java/conversionTest.java
+++ bootcamp.java/conversionTest.java
@@ -1,10 +1,14 @@
 public class conversionTest{
-    public static void main(String[] args){
+    public static void main(String[] args)
+    
+    {
         // int x = 10;
         
         short x = 10000;
         byte y = (byte)x;
 
         System.out.println(y);
+
     }
+
 }
