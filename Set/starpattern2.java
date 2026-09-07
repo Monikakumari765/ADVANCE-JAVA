075cbf0f1e2dba74e2890ab002fe5e90f8c34b2b
Monika kumari <monikakumari4687@gmail.com>
2026-09-07 10:31:54 +0530

    practice

--- Set/starpattern2.java
+++ Set/starpattern2.java
@@ -1,26 +1,25 @@
-public class starpattern {
+public class starpattern2 {
     public static void main(String[] args) {
 
         int n = 4;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) 
+        {
 
             // spaces
-
-            for (int j = 1; j <= n - i; j++) 
+            for (int j = 1; j <= i; j++)
+                
             {
                 System.out.print("    ");
             }
 
             // stars
-            
-            for (int j = 1; j <= i; j++)
+            for (int k = i; k <= n; k++) 
             {
                 System.out.print("*   ");
             }
 
             System.out.println();
         }
-
     }
 }
