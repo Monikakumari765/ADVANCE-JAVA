c41ae9e56b84930da5d161d3398e544b7148952f
Monika kumari <monikakumari4687@gmail.com>
2026-09-07 11:17:22 +0530

    practice

--- Set/uphillpattern.java
+++ Set/uphillpattern.java
@@ -1,26 +1,23 @@
 public class uphillpattern {
     public static void main(String[] args) {
 
-        int n = 3;
+        int n = 5;
 
         for (int i = 1; i <= n; i++)
          {
-
             // spaces ke liye loop
-            for (int j = 1; j <= n - i; j++)
-
+            for (int j = i; j <= n; j++)
             {
-                System.out.print(" ");
+                System.out.print("  ");
             }
-
-            // stars ke liye loop
-            System.out.print(" ");
-            for (int j = 1; j <= 2 * i - 1; j++) 
-            
+            for (int k = 1; k <= i; k++) 
             {
                 System.out.print("* ");
             }
-
+            for(int l=2; l<=i; l++)
+            {
+                System.out.print("* ");
+            }
             System.out.println();
         }
     }
