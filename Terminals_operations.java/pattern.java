4e8ad746da78292ea88b982f1e7c2797a6ae80cf
Monika kumari <monikakumari4687@gmail.com>
2026-09-07 09:42:04 +0530

    practice

--- Terminals_operations.java/pattern.java
+++ Terminals_operations.java/pattern.java
@@ -1,7 +1,7 @@
 public class pattern {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 3; i++) 
+        for (int i = 1; i <= 5; i++) 
             {
             for (int j = 1; j <= 5; j++) 
                 {
