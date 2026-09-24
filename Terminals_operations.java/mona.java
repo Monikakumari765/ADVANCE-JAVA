f5d861c7cedac9e13cf17a15bba12ed4715fc429
Monika kumari <monikakumari4687@gmail.com>
2026-09-24 09:54:26 +0530

    practice

---
+++ Terminals_operations.java/mona.java
@@ -0,0 +1,10 @@
+public class mona {
+    public static void main(String[] args) {
+
+        String str = "Monika";
+
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println("Index " + i + " = " + str.charAt(i));
+        }
+    }
+}
