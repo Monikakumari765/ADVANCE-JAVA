7331ca6476e9b4fa1302b5c0d258fee8d257f911
Monika kumari <monikakumari4687@gmail.com>
2026-09-25 10:23:43 +0530

    practice

---
+++ Set/startwith.java
@@ -0,0 +1,16 @@
+public class startwith {
+    public static void main(String[] args) {
+
+        String s = "Monika Kumari";
+
+        System.out.println(s.startsWith("Monika"));
+        System.out.println(s.startsWith("Kumari"));
+        System.out.println(s.startsWith("kriti"));
+        System.out.println(s.startsWith("puja kumari"));
+
+
+
+    }
+}
+
+
