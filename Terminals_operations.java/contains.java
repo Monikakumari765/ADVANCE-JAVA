d069797f38db50124690c5a60d0f791f488eb97d
Monika kumari <monikakumari4687@gmail.com>
2026-09-25 10:20:40 +0530

    practice

---
+++ Terminals_operations.java/contains.java
@@ -0,0 +1,10 @@
+public class contains {
+    public static void main(String[] args) {
+
+        String s = "Monika Kumari is a girl";
+
+        System.out.println(s.contains("Monika"));
+        System.out.println(s.contains("Kumari"));
+        System.out.println(s.contains("mnb"));
+    }
+}
---
+++ Terminals_operations.java/indexof.java
@@ -0,0 +1,11 @@
+public class indexof {
+    public static void main(String[] args) {
+
+        String s = "Monika kumari";
+
+        System.out.println(s.indexOf('M'));
+        System.out.println(s.indexOf('n'));
+        System.out.println(s.indexOf("nik"));
+        
+    }
+}
